3bf2f715f7324f1ce7133dab88147ddef45c007f
rohan future devoloper <xrohaislam373@gmail.com>
2023-11-12 23:29:42 +0600

    service section complete

--- src/Page/Home/Banner/Banner.jsx
+++ src/Page/Home/Banner/Banner.jsx
@@ -1,20 +1,20 @@
 import banner from "../../../assets/images/beautiful-young-asian-woman-touching-her-clean-face-with-fresh-healthy-skin-isolated-white-wall-beauty-cosmetics-facial-treatment-concept 1.png";
 const Banner = () => {
   return (
-    <div>
-      <div className="flex justify-center items-center">
-        <div className="w-1/2 ml-[134px] gap-16 py-6">
-          <h2 className="text-4xl font-bold text-black ">
+    <div className="bg-pink-50">
+      <div className="flex justify-center items-center gap-8">
+        <div className="w-1/2 ml-[134px]">
+          <h2 className="text-4xl font-bold text-black py-6">
             BEAUTY SALON <br /> FOR EVERY WOMEN
           </h2>
-          <p>
+          <p className="pb-6">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus
             commodo ipsum duis laoreet maecenas. Feugiat{" "}
           </p>
-          <button className="btn bg-pink-600">Get on Appointment</button>
+          <button className="btn bg-pink-600 text-white">Get on Appointment</button>
         </div>
         <div className="w-1/2 mr-[217px]">
-          <img src={banner} alt="" />
+          <img className="w-[484px] h-[478px]" src={banner} alt="" />
         </div>
       </div>
     </div>
--- src/Page/Home/Home/Home.jsx
+++ src/Page/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../Banner/Banner";
+import Service from "../Service/Service";
 
 const Home = () => {
-    return (
-        <div className="mb-20">
-<Banner></Banner>
-        </div>
-    );
+  return (
+    <div className="mb-20">
+      <Banner></Banner>
+      <Service></Service>
+    </div>
+  );
 };
 
 export default Home;
